76bd0b48e4c4c3ce9d81010a014d5b5b89c4de7a
lucasarai <luca.sarai1999@gmail.com>
2026-05-06 18:18:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -210,3 +210,4 @@ def api_single_check():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     app.run(debug=False, host='0.0.0.0', port=port)
+    # fix riavvio variabili
